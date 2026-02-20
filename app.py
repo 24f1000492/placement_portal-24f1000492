4a484c834ddacefa200f8e7449134c127483578a
24f1000492 <24f1000492@ds.study.iitm.ac.in>
2026-02-20 19:36:34 +0530

    login commit

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from backend.models import db
+
 app = None
 def create_app():
     app=Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///placement_portal.db.sqlite3'
+    db.init_app(app)
+    app.app_context().push()
     return app
+
+
 create_app()
+
+from backend.routes import*
 
 if __name__ == '__main__':
     app.run(debug=True)
